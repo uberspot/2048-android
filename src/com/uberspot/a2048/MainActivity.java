87fbd98f64aa1aa869022e88f4b53b4f4289daa6
uberspot <onexemailx@gmail.com>
2014-03-15 14:52:14 +0200

    made fullscreen, added saving of state, made a bit more compact

--- src/com/uberspot/a2048/MainActivity.java
+++ src/com/uberspot/a2048/MainActivity.java
@@ -5,26 +5,57 @@ import com.uberspot.a2048.R;
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private WebView mWebView;
+	private long mLastPress;
+	private static final long mBackPressThreshold = 4000;
+	
 	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		WebView webView = (WebView) findViewById(R.id.mainWebView);
-		webView.getSettings().setJavaScriptEnabled(true);
-		webView.loadUrl("file:///android_asset/2048/index.html");
+		mWebView = (WebView) findViewById(R.id.mainWebView);
+		mWebView.getSettings().setJavaScriptEnabled(true);
+		mWebView.getSettings().setDomStorageEnabled(true);
+		
+		if (savedInstanceState != null) {
+			mWebView.restoreState(savedInstanceState);
+	    } else {
+	    	mWebView.loadUrl("file:///android_asset/2048/index.html");
+	    }
 	}
 
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		mWebView.saveState(outState);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	
+	@Override
+	public void onBackPressed() {
+	    long currentTime = System.currentTimeMillis();
+	    Toast pressBackToast = Toast.makeText(getApplicationContext(), 
+        					R.string.press_back_again_to_exit, Toast.LENGTH_SHORT);
+	    if (currentTime - mLastPress > mBackPressThreshold) {
+	        pressBackToast.show();
+	        mLastPress = currentTime;
+	    } else {
+	    	pressBackToast.cancel();
+	        super.onBackPressed();
+	    }
+	}
 }
