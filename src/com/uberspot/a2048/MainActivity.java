9fcdfb7367ebbffe2ba53b10b0d1f9505e885519
uberspot <onexemailx@gmail.com>
2014-03-28 23:55:48 +0200

    make fullscreen toggleable with long touch

--- src/com/uberspot/a2048/MainActivity.java
+++ src/com/uberspot/a2048/MainActivity.java
@@ -3,9 +3,17 @@ package com.uberspot.a2048;
 import com.uberspot.a2048.R;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Toast;
@@ -13,17 +21,26 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
 	private WebView mWebView;
-	private long mLastPress;
+	private long mLastBackPress;
 	private static final long mBackPressThreshold = 4000;
+	private static final String IS_FULLSCREEN_PREF = "is_fullscreen_pref";
+	private static boolean DEF_FULLSCREEN = true;
+	private long mLastTouch;
+	private static final long mTouchThreshold = 2000;
 	
 	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		applyFullScreen(isFullScreen());
+		
 		setContentView(R.layout.activity_main);
+		
 		mWebView = (WebView) findViewById(R.id.mainWebView);
 		WebSettings settings = mWebView.getSettings();
-		String packageName = "com.uberspot.a2048";
+		String packageName = getPackageName();
 		settings.setJavaScriptEnabled(true);
 		settings.setDomStorageEnabled(true);
 		settings.setDatabaseEnabled(true);
@@ -34,6 +51,28 @@ public class MainActivity extends Activity {
 	    } else {
 	    	mWebView.loadUrl("file:///android_asset/2048/index.html");
 	    }
+		Toast.makeText(getApplication(), 
+				R.string.toggle_fullscreen, Toast.LENGTH_SHORT).show();
+		mWebView.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				// Implement a long touch action by comparing
+				// time between action up and action down
+				long currentTime = System.currentTimeMillis();
+				if (event.getAction() == MotionEvent.ACTION_MOVE) {
+					return false;
+				} else if(event.getAction() == MotionEvent.ACTION_UP &&
+						Math.abs(currentTime - mLastTouch) > mTouchThreshold) {
+			    	boolean toggledFullScreen = !isFullScreen();
+					saveFullScreen(toggledFullScreen);
+		        	applyFullScreen(toggledFullScreen);
+			    } else if(event.getAction() == MotionEvent.ACTION_DOWN) {
+			    	mLastTouch = currentTime;
+			    }
+				// return so that the event isn't consumed but used
+				// by the webview as well
+				return false;
+			}});
 	}
 
 	@Override
@@ -44,19 +83,40 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		//getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 	
+	private void saveFullScreen(boolean isFullScreen) {
+		// save in preferences
+		SharedPreferences.Editor editor = PreferenceManager
+								.getDefaultSharedPreferences(this).edit();
+	    editor.putBoolean(IS_FULLSCREEN_PREF, isFullScreen);
+	    editor.commit();
+	}
+
+	private boolean isFullScreen() {
+		return PreferenceManager
+				.getDefaultSharedPreferences(this)
+				.getBoolean(IS_FULLSCREEN_PREF, DEF_FULLSCREEN);
+	}
 	
+	private void applyFullScreen(boolean isFullScreen) {
+		if(isFullScreen) {
+			getWindow().clearFlags(LayoutParams.FLAG_FULLSCREEN);
+		} else {
+	        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+	            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		}
+	}
 	@Override
 	public void onBackPressed() {
 	    long currentTime = System.currentTimeMillis();
 	    Toast pressBackToast = Toast.makeText(getApplicationContext(), 
         					R.string.press_back_again_to_exit, Toast.LENGTH_SHORT);
-	    if (Math.abs(currentTime - mLastPress) > mBackPressThreshold) {
+	    if (Math.abs(currentTime - mLastBackPress) > mBackPressThreshold) {
 	        pressBackToast.show();
-	        mLastPress = currentTime;
+	        mLastBackPress = currentTime;
 	    } else {
 	    	pressBackToast.cancel();
 	        super.onBackPressed();
