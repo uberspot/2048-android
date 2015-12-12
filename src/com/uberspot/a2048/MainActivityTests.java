1443d4b1467ffc28b774e7505c0a70f2353692f0
Johnny Ting Shi <johnny.t.shi@gmail.com>
2015-12-11 22:22:22 -0800

    Add tests

---
+++ src/com/uberspot/a2048/MainActivityTests.java
@@ -0,0 +1,18 @@
+package com.uberspot.a2048;
+
+/**
+ * Created by tingshi on 12/11/15.
+ */
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTests extends ActivityInstrumentationTestCase2<MainActivity> {
+    public MainActivityTests() {
+        super(MainActivity.class);
+    }
+
+    public void testActivityExists() {
+        MainActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
